949d475d6bf901a7ca8579009a6a58a14af52f3e
Ipmake <niko.denis727@gmail.com>
2026-01-23 15:17:24 +0100

    feat: Integrate OpenPanel for analytics and error tracking

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,35 @@ import theme from './theme';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import SearchPage from './pages/SearchPage';
 import InfoPage from './pages/InfoPage';
+import { OpenPanel } from '@openpanel/web';
+import { useEffect } from 'react';
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const op = new OpenPanel({
+  clientId: '23bab4b6-1e09-4d5e-9a54-371067c45d06',
+  apiUrl: 'https://inquiry2.ipmake.dev/api',
+  trackScreenViews: true,
+  trackOutgoingLinks: true,
+  trackAttributes: true,
+});
 
 function App() {
   // const muiTheme = useTheme();
   // const isMobile = useMediaQuery(muiTheme.breakpoints.down('md'));
+
+  useEffect(() => {
+    op.init();
+
+    window.addEventListener('error', (event) => {
+      op.track('page_error', {
+        message: event.message,
+        filename: event.filename,
+        lineno: event.lineno,
+        colno: event.colno,
+        error: event.error ? event.error.toString() : 'null',
+      });
+    });
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
